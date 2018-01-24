b9f032e51a24b19eebba300f9c387680d6e324ba
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-01-23 17:26:28 -0800

    Persist :quiet through :dirs

--- src/leiningen/modules.clj
+++ src/leiningen/modules.clj
@@ -169,6 +169,7 @@ to a newline delimited list of namespaces.
 Accepts '-q', '--quiet' and ':quiet' to suppress non-subprocess output."
   [project & args]
   (let [[quiet? args] ((juxt some remove) #{"-q" "--quiet" ":quiet"} args)
+        quiet? (or quiet? (-> project :modules :quiet))
         {:keys [quiet?] :as opts} {:quiet? (boolean quiet?)}]
     (condp = (first args)
     ":checkouts" (do
@@ -177,7 +178,8 @@ Accepts '-q', '--quiet' and ':quiet' to suppress non-subprocess output."
     ":dirs" (let [dirs (s/split (second args) #"[:,]")]
               (apply modules
                 (-> project
-                  (assoc-in [:modules :dirs] dirs)
+                    (assoc-in [:modules :dirs] dirs)
+                    (assoc-in [:modules :quiet] quiet?)
                   (vary-meta assoc-in [:without-profiles :modules :dirs] dirs))
                 (drop 2 args)))
 
